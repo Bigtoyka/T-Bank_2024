b0d1c109ec31e3bab5770a19005e3460c93df6f0
Bigtoyka <ru.gosha@mail.ru>
2024-09-03 22:18:55 +0300

    method has been redesigned in accordance with the technical specifications

--- src/main/java/org/tbank/City.java
+++ src/main/java/org/tbank/City.java
@@ -1,10 +1,15 @@
 package main.java.org.tbank;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.File;
 import java.util.Map;
 
 @Getter
@@ -14,4 +19,26 @@ import java.util.Map;
 public class City {
     private String slug;
     private Map coords;
+
+    public void toXML(File file) {
+        Logger logger = LoggerFactory.getLogger(Main.class);
+        ObjectMapper objectMapper = new ObjectMapper();
+        try {
+            logger.info("Starting to read JSON file.");
+            File fileout = new File("src/main/java/org/tbank/output.xml");
+            City json = objectMapper.readValue(file, City.class);
+            logger.info("Successfully read JSON file", json);
+            logger.debug("Coordinates: {}", json.getCoords());
+            logger.info("Starting to write XML file.");
+            XmlMapper xmlMapper = new XmlMapper();
+            if (fileout.exists()) {
+                logger.warn("File already exists");
+            }
+            xmlMapper.writeValue(fileout, json);
+            logger.info("Successfully wrote XML file.");
+        } catch (Exception e) {
+            logger.error("Error in JSON file", e);
+            throw new RuntimeException(e);
+        }
+    }
 }
--- src/main/java/org/tbank/Main.java
+++ src/main/java/org/tbank/Main.java
@@ -8,26 +8,9 @@ import org.slf4j.LoggerFactory;
 import java.io.File;
 
 public class Main {
-    private static final Logger logger = LoggerFactory.getLogger(Main.class);
     public static void main(String[] args) {
-        ObjectMapper objectMapper = new ObjectMapper();
-        try {
-            logger.info("Starting to read JSON file.");
-            File file = new File("src/main/java/org/tbank/city.json");
-            File fileout = new File("src/main/java/org/tbank/output.xml");
-            City json = objectMapper.readValue(file, City.class);
-            logger.info("Successfully read JSON file", json);
-            logger.debug("Coordinates: {}", json.getCoords());
-            logger.info("Starting to write XML file.");
-            XmlMapper xmlMapper = new XmlMapper();
-            if(fileout.exists()){
-                logger.warn("File already exists");
-            }
-            xmlMapper.writeValue(fileout, json);
-            logger.info("Successfully wrote XML file.");
-        } catch (Exception e) {
-            logger.error("Error in JSON file", e);
-            throw new RuntimeException(e);
-        }
+        City city = new City();
+        File file = new File("src/main/java/org/tbank/city.json");
+        city.toXML(file);
     }
 }
