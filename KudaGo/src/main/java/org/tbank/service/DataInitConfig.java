9cf6d37184fa632f82006a4ab248971df9e5e853
Bigtoyka <ru.gosha@mail.ru>
2024-10-16 22:58:35 +0300

    Revert "сделано из ТЗ п1 и п2."
    This reverts commit 690bb1be1257abdadbd8cf81c3a0a807c5d393ac.

--- KudaGo/src/main/java/org/tbank/service/DataInitConfig.java
+++ KudaGo/src/main/java/org/tbank/service/DataInitConfig.java
@@ -2,7 +2,6 @@ package org.tbank.service;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -13,101 +12,51 @@ import org.tbank.dao.UniversalDAO;
 import org.tbank.models.Category;
 import org.tbank.models.Location;
 
-import java.time.Duration;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
-import java.util.concurrent.*;
 
 @Slf4j
 @Configuration
+@AllArgsConstructor
 public class DataInitConfig {
     private final UniversalDAO<Integer, Category> categoryDAO;
     private final UniversalDAO<String, Location> locationDAO;
     private final KudaGoClient kudaGoClient;
-    private final ExecutorService fixedThreadPool;
-    private final ExecutorService scheduledThreadPool;
-    private final Duration initializationSchedule;
 
-    public DataInitConfig(UniversalDAO<Integer, Category> categoryDAO,
-                          UniversalDAO<String, Location> locationDAO,
-                          KudaGoClient kudaGoClient,
-                          @Qualifier("fixedThreadPool") ExecutorService fixedThreadPool,
-                          @Qualifier("scheduledThreadPool") ExecutorService scheduledThreadPool,
-                          Duration initializationSchedule) {
-        this.categoryDAO = categoryDAO;
-        this.locationDAO = locationDAO;
-        this.kudaGoClient = kudaGoClient;
-        this.fixedThreadPool = fixedThreadPool;
-        this.scheduledThreadPool = scheduledThreadPool;
-        this.initializationSchedule = initializationSchedule;
-    }
 
     @Bean
     @TimeExecution
     public CommandLineRunner doInit(RestTemplate restTemplate) {
         return args -> {
             log.info("Старт инициализации данных");
-            initializeData();
-        };
-    }
-
-    private void initializeData() {
-        if (scheduledThreadPool instanceof ScheduledExecutorService) {
-            ScheduledExecutorService scheduler = (ScheduledExecutorService) scheduledThreadPool;
-            scheduler.schedule(this::doInitialization, initializationSchedule.toMillis(), TimeUnit.MILLISECONDS);
-        } else {
-            throw new IllegalStateException("scheduledThreadPool должен быть ScheduledExecutorService");
-        }
-    }
-
-    private void doInitialization() {
-        long startTime = System.currentTimeMillis();
-        List<Future<Void>> futures = new ArrayList<>();
-        futures.add(fixedThreadPool.submit(this::initializeCategories));
-        futures.add(fixedThreadPool.submit(this::initializeLocations));
-        for (Future<Void> future : futures) {
             try {
-                future.get();
-            } catch (InterruptedException | ExecutionException e) {
-                log.error("Ошибка при инициализации данных: ", e);
+                log.info("Инициализация категорий");
+                Optional<Category[]> categories = kudaGoClient.requestCategories();
+                categories.ifPresent(cat -> {
+                    for (Category category : cat) {
+                        categoryDAO.put(category.getId(), category);
+                        log.info("Инициализированные категории: {}", category.getId());
+                    }
+                    log.info("Категории успешно инициализированы");
+                });
+            } catch (Exception e) {
+                log.error("Ошибка инициализации категорий: ", e);
             }
-        }
-        long endTime = System.currentTimeMillis();
-        log.info("Инициализация данных завершена за {} миллисекунд.", (endTime - startTime));
-    }
 
-    private Void initializeCategories() {
-        try {
-            log.info("Инициализация категорий");
-            Optional<Category[]> categories = kudaGoClient.requestCategories();
-            categories.ifPresent(cat -> {
-                for (Category category : cat) {
-                    categoryDAO.put(category.getId(), category);
-                    log.info("Инициализированные категории: {}", category.getId());
-                }
-                log.info("Категории успешно инициализированы");
-            });
-        } catch (Exception e) {
-            log.error("Ошибка инициализации категорий: ", e);
-        }
-        return null;
-    }
+            try {
+                log.info("Инициализация локаций.");
+                Optional<Location[]> locations = kudaGoClient.requestLocation();
+                locations.ifPresent(loc -> {
+                    for (Location location : loc) {
+                        locationDAO.put(location.getSlug(), location);
+                        log.info("Инициализированные локации: {}", location.getSlug());
+                    }
+                    log.info("Локации успешно инициализированы");
+                });
+            } catch (Exception e) {
+                log.error("Ошибка инициализации локаций: ", e);
+            }
 
-    private Void initializeLocations() {
-        try {
-            log.info("Инициализация локаций.");
-            Optional<Location[]> locations = kudaGoClient.requestLocation();
-            locations.ifPresent(loc -> {
-                for (Location location : loc) {
-                    locationDAO.put(location.getSlug(), location);
-                    log.info("Инициализированные локации: {}", location.getSlug());
-                }
-                log.info("Локации успешно инициализированы");
-            });
-        } catch (Exception e) {
-            log.error("Ошибка инициализации локаций: ", e);
-        }
-        return null;
+            log.info("Инициализация данных завершена");
+        };
     }
 }
