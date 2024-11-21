7dad4abbedf6acc26eb536e6751cf33075d88c46
Bigtoyka <ru.gosha@mail.ru>
2024-11-21 23:43:04 +0300

    Добавлено часть тестов

---
+++ src/test/java/RabbitMQSimple.java
@@ -0,0 +1,59 @@
+import com.rabbitmq.client.BuiltinExchangeType;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import org.openjdk.jmh.annotations.*;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+@BenchmarkMode(Mode.Throughput)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@State(Scope.Thread)
+public class RabbitMQSimple {
+    private Connection connection;
+    private Channel producerChannel;
+    private Channel consumerChannel;
+    private static final String EXCHANGE = "test-exchange";
+    private static final String QUEUE = "test-queue";
+    private static final String ROUTING_KEY = "test-key";
+
+    @Setup(Level.Trial)
+    public void setUp() throws IOException, TimeoutException {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        connection = factory.newConnection();
+        producerChannel = connection.createChannel();
+        producerChannel.exchangeDeclare(EXCHANGE, BuiltinExchangeType.DIRECT, true);
+        producerChannel.queueDeclare(QUEUE, true, false, false, null);
+        producerChannel.queueBind(QUEUE, EXCHANGE, ROUTING_KEY);
+
+        consumerChannel = connection.createChannel();
+        consumerChannel.basicQos(1);
+        consumerChannel.basicConsume(QUEUE, true, (consumerTag, delivery) -> {
+            String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
+        }, consumerTag -> {});
+
+    }
+
+    @TearDown
+    public void tearDown() throws IOException, TimeoutException {
+        if (producerChannel != null && producerChannel.isOpen()) {
+            producerChannel.close();
+        }
+        if (consumerChannel != null && consumerChannel.isOpen()) {
+            consumerChannel.close();
+        }
+        if (connection != null && connection.isOpen()) {
+            connection.close();
+        }
+    }
+
+    @Benchmark
+    public void testProducerConsumer() throws IOException {
+        String msg = "SIMPLE-TEST";
+        producerChannel.basicPublish(EXCHANGE, ROUTING_KEY, null, msg.getBytes(StandardCharsets.UTF_8));
+    }
+}
---
+++ src/test/java/RabbitTestLoadBalancing.java
@@ -0,0 +1,70 @@
+import com.rabbitmq.client.BuiltinExchangeType;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import org.openjdk.jmh.annotations.*;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
+
+@BenchmarkMode(Mode.Throughput)
+@OutputTimeUnit(TimeUnit.MILLISECONDS)
+@State(Scope.Thread)
+public class RabbitTestLoadBalancing {
+    private Connection connection;
+    private Channel[] producerChannels;
+    private Channel consumerChannel;
+    private static final String EXCHANGE = "test-exchange";
+    private static final String QUEUE = "test-queue";
+    private static final String ROUTING_KEY = "test-key";
+    private static final int PRODUCER_COUNT = 3;
+
+    @Setup(Level.Trial)
+    public void setUp() throws IOException, TimeoutException {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        connection = factory.newConnection();
+
+        producerChannels = new Channel[PRODUCER_COUNT];
+        for (int i = 0; i < PRODUCER_COUNT; i++) {
+            Channel channel = connection.createChannel();
+            channel.exchangeDeclare(EXCHANGE, BuiltinExchangeType.DIRECT, true);
+            channel.queueDeclare(QUEUE, true, false, false, null);
+            channel.queueBind(QUEUE, EXCHANGE, ROUTING_KEY);
+            producerChannels[i] = channel;
+        }
+
+        consumerChannel = connection.createChannel();
+        consumerChannel.basicQos(1);
+
+        consumerChannel.basicConsume(QUEUE, true, (consumerTag, delivery) -> {
+            String message = new String(delivery.getBody(), StandardCharsets.UTF_8);
+        }, consumerTag -> {
+        });
+    }
+
+    @TearDown(Level.Trial)
+    public void tearDown() throws IOException, TimeoutException {
+        for (Channel producerChannel : producerChannels) {
+            if (producerChannel != null && producerChannel.isOpen()) {
+                producerChannel.close();
+            }
+        }
+        if (consumerChannel != null && consumerChannel.isOpen()) {
+            consumerChannel.close();
+        }
+        if (connection != null && connection.isOpen()) {
+            connection.close();
+        }
+    }
+
+    @Benchmark
+    public void testProducerConsumer() throws IOException {
+        String message = "LOAD BALANCING TEST";
+        for (Channel producerChannel : producerChannels) {
+            producerChannel.basicPublish(EXCHANGE, ROUTING_KEY, null, message.getBytes(StandardCharsets.UTF_8));
+        }
+    }
+}
